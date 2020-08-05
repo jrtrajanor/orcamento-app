d76b30be2054214723230932c03794916cb63d6e
Júnior Trajano Rodrigues <junior.rodrigues@softplan.com.br>
2020-08-04 23:28:49 -0300

    Commit inicial

---
+++ build/precache-manifest.c245f0accbb78786bb87cd78599ed1f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44944b8c23cc07d707891ff75d40e353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f18e355e40261e33faf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64c3b358b17cf9827c02",
+    "url": "/static/js/2.3c654a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c654a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f18e355e40261e33faf",
+    "url": "/static/js/main.eb5a5b2d.chunk.js"
+  },
+  {
+    "revision": "15d83325e20549b24394",
+    "url": "/static/js/runtime-main.ced2a1a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
